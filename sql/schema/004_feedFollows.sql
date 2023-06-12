b85db9a372eb3dafd63392e7ccf701e6b8f42bf5
mustafa <mustafamcm9@gmail.com>
2023-06-13 01:55:56 +0300

    remove deleteoncascade

--- sql/schema/004_feedFollows.sql
+++ sql/schema/004_feedFollows.sql
@@ -6,7 +6,7 @@ CREATE TABLE feedFollows (
   user_id UUID NOT NULL,
   feed_id UUID NOT NULL, 
   FOREIGN KEY(user_id) REFERENCES users(id),
-  FOREIGN KEY(feed_id) REFERENCES feeds(id) ON DELETE CASCADE
+  FOREIGN KEY(feed_id) REFERENCES feeds(id)
 );
 
 -- +goose Down
